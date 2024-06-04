9743302d69357df23000ba26479a4a9cc301292d
kevinnadr <kevin19305.ib@gmail.com>
2024-06-04 17:26:52 +0700

    mendapatkan setiap karakter dalam satu baris

--- iofile.cpp
+++ iofile.cpp
@@ -16,6 +16,8 @@ int main() {
     //unlimited loop untuk menulis
     while (true) {
         cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
         
     }
 
